66e007af2c7b14576f48ad737c1031e27fe51d05
Camilo Sanchez <camilin87@gmail.com>
2020-02-20 17:05:52 -0500

    Make sure the ArrayList is a Collection

--- src/main/java/com/tddapps/datastructures/ArrayList.java
+++ src/main/java/com/tddapps/datastructures/ArrayList.java
@@ -1,5 +1,81 @@
 package com.tddapps.datastructures;
 
-public class ArrayList<T> {
-//    TODO implement this
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ArrayList<T> implements Collection<T> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
